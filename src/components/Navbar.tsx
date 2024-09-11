67fdb9c9ae9038f6f6998da19419632f2babeff7
JDPM <josedanielpmj@gmail.com>
2024-09-11 08:16:31 -0400

    10-09-2024.18

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -29,7 +29,7 @@ function Navbar() {
     <>
       <nav className="navbar navbar-expand-lg bg-dark navbar-dark">
         <div className="container-fluid">
-          <a className="navbar-brand" href="/">Inventary System</a>
+          <a className="navbar-brand" href="/">Inventory System</a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
